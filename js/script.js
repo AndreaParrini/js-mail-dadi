5a1df53a764efa8cfb54835e987f10511a3d6d4e
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-25 15:37:28 +0100

    add function to control function
    button annulla, tryagain, registration
    and create form in html

--- js/script.js
+++ js/script.js
@@ -11,14 +11,14 @@ let userMail = "";
 
 // Salvo il valore che l'utente ha inserito in una variabile, quando clicca sul tasto login
 
-document.getElementById('login').addEventListener('click', function(){
+document.getElementById('login').addEventListener('click', function () {
 
     //assegno il valore alla variabile creata in precedenza
     userMail = document.getElementById('usermail').value;
     console.log(userMail);
 
     // creo una variabile per capire se la mail inserita è presente oppure no nella lista, e la valorizzo subito con false
-    let controlAccess = false; 
+    let controlAccess = false;
 
     //scorro nella lista
     for (let i = 0; i < mailRegistered.length; i++) {
@@ -27,44 +27,67 @@ document.getElementById('login').addEventListener('click', function(){
         console.log(element);
 
         // confronto la mail inserita dall'utente con le mail della lista
-        if( element === userMail ){
+        if (element === userMail) {
 
             // se è presente, e quindi una mail nella lista è uguale a quella inserita metto la variabile a true 
             controlAccess = true;
 
         }
-        
+
     }
 
     // controllo la variabile se true faccio accedere l'utente altrimenti no
-    if(controlAccess === true) {
+    if (controlAccess === true) {
 
         document.getElementById('sectionLogin').classList.add('d-none');
         document.getElementById('sectionPlay').classList.remove('d-none');
-        document.getElementById('nameUser').innerHTML = userMail;
+        document.getElementById('user').innerHTML = userMail;
 
-    } else{
-        alert('Prima di accedere al sito devi registrarti')
+    } else {
+        document.getElementById('sectionLogin').classList.add('d-none');
+        document.getElementById('sectionErrorMail').classList.remove('d-none');
+        document.getElementById('nameUser').innerHTML = userMail;
     }
     console.log(controlAccess);
 
 })
 
-document.getElementById('play').addEventListener('click', function(){
+document.getElementById('annulla').addEventListener('click', function () {
 
-    const userNumber = Math.round(Math.random()*5)+1;
+    document.getElementById('usermail').value = "";
+
+})
+
+document.getElementById('tryagain').addEventListener('click', function () {
+
+    document.getElementById('sectionLogin').classList.remove('d-none');
+    document.getElementById('sectionErrorMail').classList.add('d-none');
+    document.getElementById('usermail').value = userMail;
+
+})
+
+document.getElementById('registration').addEventListener('click', function(){
+
+    document.getElementById('sectionRegistration').classList.remove('d-none');
+    document.getElementById('sectionErrorMail').classList.add('d-none');
+
+})
+
+document.getElementById('play').addEventListener('click', function () {
+
+    const userNumber = Math.round(Math.random() * 5) + 1;
     document.getElementById('usernumber').innerHTML = userNumber;
     console.log(userNumber);
 
-    const computerNumber = Math.round(Math.random()*5)+1;
+    const computerNumber = Math.round(Math.random() * 5) + 1;
     document.getElementById('computernumber').innerHTML = computerNumber;
     console.log(computerNumber);
 
-    if(userNumber > computerNumber){
+    if (userNumber > computerNumber) {
         alert(`Complienti ${userMail} hai vinto`);
-    } else if(userNumber < computerNumber){
+    } else if (userNumber < computerNumber) {
         alert(`Mi dispiace ${userMail} hai perso`);
-    }else{
+    } else {
         alert('La partita è finita in parità')
     }
 
